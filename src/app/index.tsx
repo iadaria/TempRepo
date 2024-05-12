8bc6c5d6bcce8d6485d9dacabe9705771f99d093
iadaria <jadarya@mail.ru>
2024-05-12 18:04:50 +0300

    update to 74

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { CounterScreeen } from '../screens/counter/ui/CounterScreeen';
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { store } from './providers/StoreProvider/config/store';
 
 function App(): React.JSX.Element {
   return (
