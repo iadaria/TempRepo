0e8c03e851aec4b10bbc8fe8dd32a63daba275ea
iadaria <jadarya@mail.ru>
2024-06-06 08:53:44 +0300

    come back interface

--- src/shared/ui/Logo/LogoStyle.ts
+++ src/shared/ui/Logo/LogoStyle.ts
@@ -2,7 +2,7 @@ import { colors } from '@src/shared/lib/theme';
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  logo: { flex: 0.9, alignItems: 'center' },
+  logo: { flex: 1, alignItems: 'center' },
   logoTitle: {
     fontFamily: 'Viga-Regular',
     color: colors.secondary,
