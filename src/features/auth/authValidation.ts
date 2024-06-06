6a6431c243b054b22d71434237bbb9be82474464
iadaria <jadarya@mail.ru>
2024-06-06 09:57:42 +0300

    add auth rules

--- src/features/auth/authValidation.ts
+++ src/features/auth/authValidation.ts
@@ -1,6 +1,6 @@
 import { object, string } from 'yup';
 
 export const loginSchema = object({
-  email: string().required('Email is a required field'),
-  password: string().required('Password is a required field'),
+  email: string().required('Email is a required field').email(),
+  password: string().required('Password is a required field').min(6).max(36),
 });
