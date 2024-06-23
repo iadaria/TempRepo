25147125fc2bb4e74a9b7526610b801e458327af
iadaria <jadarya@mail.ru>
2024-06-23 11:48:43 +0300

    _

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -2,4 +2,4 @@ export { default as Google } from './google.svg';
 export { default as Facebook } from './facebook.svg';
 export { default as Notification } from './notify.svg';
 export { default as Notified } from './notified.svg';
-export { default as Filter } from './Filter.svg';
+export { default as Filter } from './filter.svg';
