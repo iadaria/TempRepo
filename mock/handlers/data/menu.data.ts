7e2599ab55a7fd0dfc88734b90cbe6d20c1858d2
iadaria <jadarya@mail.ru>
2024-06-27 22:51:58 +0300

    msw add menu data

---
+++ mock/handlers/data/menu.data.ts
@@ -0,0 +1,28 @@
+import { PATH } from './consts';
+
+export const MENU = [
+  {
+    id: 1,
+    name: 'Herbal Pancacke',
+    price: 7,
+    restaurantId: 1,
+    uri: `${PATH}/menu/m1.png`,
+    pupular: 1,
+  },
+  {
+    id: 2,
+    name: 'Furit Salad',
+    price: 5,
+    restaurantId: 1,
+    uri: `${PATH}/menu/m2.png`,
+    popular: 2,
+  },
+  {
+    id: 3,
+    name: 'Green Noddle',
+    price: 6,
+    restaurantId: 1,
+    uri: `${PATH}/menu/m3.png`,
+    popular: 3,
+  },
+];
