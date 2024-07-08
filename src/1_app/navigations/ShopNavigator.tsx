935bcfa3cbb69c0578f22a902dd4ebc63d481b81
iadaria <jadarya@mail.ru>
2024-07-08 17:32:06 +0300

    Check back button with animation

--- src/1_app/navigations/ShopNavigator.tsx
+++ src/1_app/navigations/ShopNavigator.tsx
@@ -1,16 +1,15 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { ShopHomeScreen } from '@src/2_screens/shop/shop-home';
 import { ShopRestaurantsScreen } from '@src/2_screens/shop/shop-restaurants/ShopRestaurantsScreen/ShopRestaurantsScreen';
-import { BackIcon } from '@src/6_shared/assets/icons';
 import { routes } from '@src/6_shared/consts/routes';
-import { Image } from 'react-native';
 
 const { shop } = routes;
 
 export const ShopNavigator = createNativeStackNavigator({
-  initialRouteName: shop.PopularRestorants,
+  //initialRouteName: shop.PopularRestorants,
   screenOptions: {
     headerShown: false,
+    animation: 'slide_from_left',
   },
   screens: {
     [shop.Home]: ShopHomeScreen,
