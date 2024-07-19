69ff3f0777e4f198f62bd68215d01503a683703e
iadaria <jadarya@mail.ru>
2024-07-19 23:04:07 +0300

    Authorization middleware

--- mock/handlers/cart.handlers.ts
+++ mock/handlers/cart.handlers.ts
@@ -2,18 +2,19 @@ import { endpoints } from '@src/6_shared/consts/endpoints';
 import { baseUrl } from '@src/6_shared/lib/api/baseUrl';
 import { log } from '@src/6_shared/lib/debug/log';
 import { db } from 'mock/db';
+import { withAuth } from 'mock/middleware';
 import { http, HttpResponse } from 'msw';
 
 const { shop } = endpoints;
 
 export const cartHandlers = [
-  http.get(baseUrl(shop.cart), ({ request }) => {
-    const userId = 1;
-    log('handler', {
-      cart: db.cart.findMany({ where: { userId: { equals: userId } } }),
-    });
-    return HttpResponse.json({
-      data: db.cart.findMany({ where: { userId: { equals: userId } } }),
-    });
-  }),
+  http.get(
+    baseUrl(shop.cart),
+    withAuth(() => {
+      const userId = 1;
+      return HttpResponse.json({
+        data: db.cart.findMany({ where: { userId: { equals: userId } } }),
+      });
+    }),
+  ),
 ];
--- src/6_shared/lib/api/_fetch.ts
+++ src/6_shared/lib/api/_fetch.ts
@@ -13,6 +13,9 @@ export async function _fetch(input: URL, init?: RequestInit) {
   const headers = { ...baseHeaders, ...init?.headers };
   const response = await fetch(input, { ...init, headers });
 
+  if (response.status == 401) {
+    Alert.alert('Authorize, please');
+  }
   /* if (response.status == 500) {
     Alert.alert('Server Error');
   } */
