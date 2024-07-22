105782fdd6c01aef7e034017de504930b750723b
iadaria <jadarya@mail.ru>
2024-07-22 12:13:21 +0300

    calc price to slice

--- src/5_entities/cart/cart.slice.ts
+++ src/5_entities/cart/cart.slice.ts
@@ -6,11 +6,17 @@ import { RootState } from '@src/1_app/providers/StoreProvider/config/store';
 export type CartState = {
   items: CartItem[];
   amount: number;
+  totalPrice: number;
+  totalDiscount: number;
+  total: number;
 };
 
 const initialState: CartState = {
   items: [],
   amount: 0,
+  totalPrice: 0,
+  totalDiscount: 0,
+  total: 0,
 };
 
 const cartSlice = createSlice({
@@ -19,7 +25,12 @@ const cartSlice = createSlice({
   reducers: {},
   extraReducers: builder => {
     builder.addCase(fetchCart.fulfilled, (state, action) => {
-      state.items = action.payload;
+      const items = action.payload;
+      state.items = items;
+      state.totalPrice = items.reduce((sum, item) => sum + item.totalPrice, 0);
+      state.totalDiscount = items.reduce((sum, item) => sum + item.discount, 0);
+      state.total =
+        state.totalPrice - (state.totalPrice / 100) * state.totalDiscount;
     });
 
     builder.addCase(fetchCartAmount.fulfilled, (state, action) => {
